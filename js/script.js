7fd42da831b3f45b9374f75435b04dcfa7029ddc
Davide Cesare <davidereaper@gmail.com>
2020-09-09 14:28:10 +0200

    Aggiunto commenti, function invio e function receive

--- js/script.js
+++ js/script.js
@@ -1,30 +1,112 @@
 $(document).ready(function () {
 
-  $(".user_n_im_i").hover(
+  // EFFETTO HOVER UTENTI
+  $(".user_n_im_i").mouseover(
     function () {
       $(this).addClass("bgcolor_grey");
     }
   );
-  $(".user_n_im_i").click(
+  $(".user_n_im_i").mouseleave(
     function () {
-      var userImg = $(".name_user > img").clone();
-      userImg.appendTo(".img_user");
+      $(this).removeClass("bgcolor_grey");
+    }
+  );
+  // $(".user_n_im_i").click(
+  //   function () {
+  //     var userImg = $(".name_user > img").clone();
+  //     userImg.find(".user_info .img_user > img").text();
+  //
+  //     var nomeCopy = $("name_user_n_lastmsg > h4").text();
+  //     var userName = $(".user_info .img_user > .user_name").clone();
+  //     userName.find(".user_info .img_user > .user_name").text(nomeCopy);
+  //   }
+  // );
 
-      var userName = $(".name_user_n_lastmsg > h4").clone();
-      userName.appendTo("p.user_name");
-
+  // AL CLICK SULLA INPUT MSG VISUALIZZO "BUTTON" INVIO E RIMUOVO "BUTTON" MICROFONO
+  $(".msg_wrtn").click(
+    function () {
+      $(".fas.fa-microphone").addClass("d_none");
+      $(".fas.fa-paper-plane").removeClass("d_none");
     }
   );
 
+  // AL CLICK SUL "BUTTON" INVIO, INVIO IL MIO MESSAGGIO
+  $(".fas.fa-paper-plane").click(
+    function () {
+      sendMessage();
+      setTimeout(receiveMessage, 2000);
+      $(".fas.fa-microphone").removeClass("d_none");
+      $(".fas.fa-paper-plane").addClass("d_none");
+    }
+  );
+
+  // AL CLICK SUL TASTO INVIO (TASTIERA), INVIO IL MIO MESSAGGIO
   $(".msg_wrtn").keydown(
     function(e) {
-    if (e.which == 13) {
-      var msgSend = $(".msg_wrtn").val();
-      msgSend.appendTo(".msg_msg");
-      $(".msg_msg").removeClass("d_none");
-      $(".msg_msg").addClass("msg_green");
-    }
+      if (e.which == 13) {
+        sendMessage();
+        setInterval(isWriting, 0000);
+        setTimeout(receiveMessage, 2000);
+        clearTimeout();
+      }
     }
   );
 
+  // RICERCA UTENTI
+  $(".input_searching").on("keyup",
+    function () {
+      var valueSearch = $(".input_searching").val().toLowerCase();
+      $(".users_list .user_n_im_i").filter(
+        function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(valueSearch) > -1)
+        });
+    }
+  );
+
+
+
+  // FUNCTIONS
+    // function setting orario completo
+  function time () {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    if (minutes < 10) {
+      var time = hours + ":" + "0" +  minutes;
+    } else {
+      var time = hours + ":" + minutes;
+    }
+    return time;
+  }
+
+    // function setting invio messaggio utente
+  function sendMessage () {
+    var inputUtente = $(".msg_wrtn").val();
+    if (inputUtente.length != "") {
+      var templateMessage = $(".templates .msg_row").clone();
+      templateMessage.find(".msg_text").text(inputUtente);
+      templateMessage.find(".hour_send").text(time());
+      templateMessage.find(".msg_msg").addClass("msg_green");
+      $(".chat").append(templateMessage);
+      $(".msg_wrtn").val("");
+    }
+  }
+
+    // function setting ricevi messaggio da parte dello user chat corrente
+  function receiveMessage () {
+    var receiveMsg = $(".templates .msg_row").clone();
+    receiveMsg.find(".msg_text").text("Ok");
+    receiveMsg.find(".hour_send").text(time());
+    receiveMsg.find(".msg_msg").addClass("msg_white");
+    $(".chat").append(receiveMsg);
+  }
+
+  function isWriting () {
+    var writeAMessage = $(".user_is_writing");
+    writeAMessage.find(".last_online").text("Sta scrivendo...");
+  }
+
 });
+
+// FUNCTIONS
